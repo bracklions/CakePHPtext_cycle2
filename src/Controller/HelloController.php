022dd9c8ba1603120141cf71774f24c9befd62c1
bracklions <deerlions@yahoo.co.jp>
2019-06-16 23:44:14 +0900

    Section2-3フォームヘルパー:20190616

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -6,19 +6,40 @@ namespace App\Controller;
 use App\Controller\AppController;
 
 class HelloController extends AppController {
-    public $autoRender = false;
-    private $data = [
-        ['name' => 'taro', 'mail' => 'taro@yamada', 'tel' => '090-999-999'],
-        ['name' => 'hanako', 'mail' => 'hanako@flower', 'tel' => '080-888-888'],
-        ['name' => 'sachiko', 'mail' => 'sachico@happy', 'tel' => '070-777-777']
-    ];
+    // public $autoRender = false;
+    // private $data = [
+    //     ['name' => 'taro', 'mail' => 'taro@yamada', 'tel' => '090-999-999'],
+    //     ['name' => 'hanako', 'mail' => 'hanako@flower', 'tel' => '080-888-888'],
+    //     ['name' => 'sachiko', 'mail' => 'sachico@happy', 'tel' => '070-777-777']
+    // ];
 
     public function index() {
-        $id = 0;
-        // $id = 'no name';
-        if (isset($this->request->query['id'])) {
-            $id = $this->request->query['id'];
+        
+        $this->viewBuilder()->autoLayout(false);
+        // 2-11
+        $this->set('title', 'Hello!');
+        // 2-14
+        if ($this->request->isPost()) {
+            $this->set('data', $this->request->data['Form1']);
+        } else {
+            $this->set('data', []);
         }
+
+        // 2-7
+        // $values = [
+        //     'title' => 'Hello!',
+        //     'message' => 'This is message!'
+        // ];
+        // $this->set($values);
+        // 2-6
+        // $this->set('title', 'Hello!');
+        // $this->set('message', 'This is message!');
+
+        // $id = 0;
+        // $id = 'no name';
+        // if (isset($this->request->query['id'])) {
+        //     $id = $this->request->query['id'];
+        // }
         // $pass = 'no password';
         // if (isset($this->request->query['pass'])) {
         //     $pass = $this->request->query['pass'];
@@ -27,6 +48,19 @@ class HelloController extends AppController {
         // echo "<ul><li>your id: ".$id."</li>";
         // echo "<li>password: ".$pass."</li></ul>";
         // echo "</body></html>";
-        echo json_encode($this->data[$id]);
+        // echo json_encode($this->data[$id]);
+    }
+
+    public function form() {
+        $this->viewBuilder()->autoLayout(false);
+        $name = $this->request->data['name'];
+        $mail = $this->request->data['mail'];
+        $age = $this->request->data['age'];
+        $res = 'こんにちは'.$name.'('.$age.')さん。メールアドレスは、'.$mail.'ですね？';
+        $values = [
+            'title' => 'Result',
+            'message' => $res
+        ];
+        $this->set($values);
     }
 }
