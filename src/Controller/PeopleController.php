89f98f7203b79645776fc0301424dc7a2ef581cc
bracklions <deerlions@yahoo.co.jp>
2019-06-19 16:20:01 +0900

    Chapter3-3finish

--- src/Controller/PeopleController.php
+++ src/Controller/PeopleController.php
@@ -8,7 +8,73 @@ use App\Controller\AppController;
 class PeopleController extends AppController {
     
     public function index() {
-        $data = $this->People->find('list');
+        // $id = $this->request->query['id'];
+        // $data = $this->People->get($id);
+        $data = $this->People->find('all');
         $this->set('data', $data);
+    }
+
+    public function add() {
+        $entity = $this->People->newEntity();
+        $this->set('entity', $entity);
+    }
+
+    public function create() {
+        if ($this->request->is('post')) {
+            // add.ctpのフォームの値を取り出す
+            $data = $this->request->data['People'];
+            // 新しいエンティティのインスタンスの作成
+            $entity = $this->People->newEntity($data);
+            // 引数$entityがデータベーステーブルに保存される
+            $this->People->save($entity);
+        }
+        // create()を実行したらindexを表示
+        return $this->redirect(['action' => 'index']);
+    }
+
+    public function edit() {
+        // クエリのidを取得する
+        $id = $this->request->query['id'];
+        // idのエンティティを取り出す
+        $entity = $this->People->get($id);
+        // setでエンティティを渡す
+        $this->set('entity', $entity);
+    }
+
+    public function update() {
+        if ($this->request->is('post')) {
+            // edit.ctpのフォームの値を取り出す
+            $data = $this->request->data['People'];
+            // idのエンティティを取り出す
+            $entity = $this->People->get($data['id']);
+            // フォームに入力した値($data)でエンティティ($entity)を更新する
+            $this->People->patchEntity($entity, $data);
+            // 更新(patch)された引数$entityがデータベーステーブルに保存される
+            $this->People->save($entity);
+        }
+        // update()を実行したらindexを表示
+        return $this->redirect(['action' => 'index']);
+    }
+
+    public function delete() {
+        // クエリのidを取得する
+        $id = $this->request->query['id'];
+        // idのエンティティを取り出す
+        $entity = $this->People->get($id);
+        // setでエンティティを渡す
+        $this->set('entity', $entity);
+    }
+
+    public function destroy() {
+        if ($this->request->is('post')) {
+            // delete.ctpのフォームの値を取り出す
+            $data = $this->request->data['People'];
+            // idのエンティティを取り出す
+            $entity = $this->People->get($data['id']);
+            // 選んだエンティティを削除する
+            $this->People->delete($entity);
+        }
+        // delete()を実行したらindexを表示
+        return $this->redirect(['action' => 'index']);
     }
 }
--- src/Template/People/index.ctp
+++ src/Template/People/index.ctp
@@ -1,4 +1,32 @@
 <p>This is People table records.</p>
-<pre>
-<?php print_r($data->toArray()); ?>
-</pre>
+<table>
+<thead>
+    <tr>
+        <th>id</th>
+        <th>name</th>
+        <th>mail</th>
+        <th>age</th>
+    </tr>
+</thead>
+<?php foreach($data->toArray() as $obj): ?>
+    <tr>
+        <td><?= h($obj->id) ?></td>
+        <td><a href="
+                    <?= $this->Url->build(['controller' => 'People',
+                                           'action' => 'edit']); ?>?id=<?= $obj->id ?>
+                    "><!-- <a href="/mycakeapp2/people/edit?id=〜 になる -->
+            <?= h($obj->name) ?>
+            </a>
+        </td>
+        <td><?= h($obj->mail) ?></td>
+        <td><?= h($obj->age) ?></td>
+        <td><a href="
+                    <?= $this->Url->build(['controller' => 'People',
+                                           'action' => 'delete']); ?>?id=<?= $obj->id ?>
+                    "><!-- <a href="/mycakeapp2/people/delete?id=〜 になる -->
+            delete
+            </a>
+        </td>
+    </tr>
+<?php endforeach; ?>
+</table>
